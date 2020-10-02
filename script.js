13c05174f38f7478112a6acf0ec8bad5a52262d9
pindellk <pindellk@protonmail.com>
2020-10-01 20:46:12 -0400

    Add OMDB AJAX call, get movie poster and title

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+// On-click event for movie search
 
+// Function for finding movie genre
+
+// Function for finding books based on subject
+
+// Function for BOOK AJAX get request
+
+// Append book results to DOM
+
+
+// Search on-click event
+$("#movie-search").click(function (event) {
+    event.preventDefault();
+
+    var movie = $("#movie-input").val().trim();
+    var APIkey = "&apikey=trilogy";
+    var queryURL = "https://www.omdbapi.com/?t=" + movie + APIkey;
+
+    localStorage.setItem("movie", movie);
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        // Pull movie title and append to DOM
+        $("#movie-title").text(response.Title);
+
+        // Pull movie poster img and append to DOM
+        $("#movie-pick").attr("src", response.Poster);
+
+        console.log();
+    });
+
+});
