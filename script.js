eb87dcace8a2f69319cb8c5913f1256257c0bd44
Roger Steve <roxhersbozali@gmail.com>
2020-10-03 10:58:28 -0400

    ignore it

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 
+var queryURL = "https://www.googleapis.com/books/v1/volumes?q=search+subject:&orderBy=newest" + book ;
+var book =  $(this).attr("data-name");
+var books = $();
+
+
+function displaybooks() {
+ $.ajax({
+     url:queryURL,
+     method: "GET"
+ }).then (function(Response) {
+  console.log(Response)
+
+var results = 0
+ 
+ })   
+}
